2d84f9a47764aa87712dbfd1c80ea600a0ddd794
Hridayesh1605 <hridayeshmore16@gmail.com>
2025-04-26 16:22:53 +0530

    created feactureSection,DiscoverSection,CallToActionSection,FooterSecture and finished landing page

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import HeroSection from './HeroSection'
+import FeactureSection from './FeactureSection'
+import DiscoverSection from './DiscoverSection'
+import CallToActionSection from './CallToActionSection'
+import FooterSection from './FooterSection'
 
 const Landing = () => {
   return (
     <div>
       <HeroSection/>
+      <FeactureSection/>
+      <DiscoverSection/>
+      <CallToActionSection/>
+      <FooterSection/>
     </div>
   )
 }
