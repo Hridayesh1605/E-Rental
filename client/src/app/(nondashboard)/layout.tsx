f18ef19f4a0d7ac20edef8f80d6d3f346d01a634
Hridayesh1605 <hridayeshmore16@gmail.com>
2025-04-29 13:01:45 +0530

    written code for getting userDetails after signup

--- client/src/app/(nondashboard)/layout.tsx
+++ client/src/app/(nondashboard)/layout.tsx
@@ -1,8 +1,15 @@
+"use client"
+
 import Navbar from '@/components/Navbar'
 import { NAVBAR_HEIGHT } from '@/lib/constants'
+import { useGetAuthUserQuery } from '@/state/api';
 import React from 'react'
 
 const layout = ({ children }: { children: React.ReactNode }) => {
+
+  const { data: authUser } = useGetAuthUserQuery();
+  console.log("Auth User", authUser)
+
   return (
     <div className='h-full w-full'>
         <Navbar/>
